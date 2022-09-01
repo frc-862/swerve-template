a4896e784cc332d58f6709927a83acd302c42ddb
im-a-robo <nmdas05x@gmail.com>
2022-09-01 18:45:55 -0400

    PROG-274 added changes to work with CANivore

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import frc.robot.lightningUtil.swervelib.CanPort;
+
 import edu.wpi.first.math.util.Units;
 
 public final class Constants {
@@ -15,24 +17,24 @@ public final class Constants {
     
     public static final double DRIVETRAIN_WHEELBASE_METERS = Units.inchesToMeters(22.375);
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 8;
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 7;
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 16;
+    public static final CanPort FRONT_LEFT_MODULE_DRIVE_MOTOR =  new CanPort(8, "Canivore");
+    public static final CanPort FRONT_LEFT_MODULE_STEER_MOTOR = new CanPort(7, "Canivore");
+    public static final CanPort FRONT_LEFT_MODULE_STEER_ENCODER = new CanPort(16, "Canivore");
     public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(269.121);// -Math.toRadians(-95.09765625d);
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 11;
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 12;
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 17;
+    public static final CanPort FRONT_RIGHT_MODULE_DRIVE_MOTOR = new CanPort(11, "Canivore");
+    public static final CanPort FRONT_RIGHT_MODULE_STEER_MOTOR = new CanPort(12, "Canivore");
+    public static final CanPort FRONT_RIGHT_MODULE_STEER_ENCODER = new CanPort(17, "Canivore");
     public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(193.447);// -Math.toRadians(-12.744140625d);
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 10;
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 9;
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 15;
+    public static final CanPort BACK_LEFT_MODULE_DRIVE_MOTOR = new CanPort(10, "Canivore");
+    public static final CanPort BACK_LEFT_MODULE_STEER_MOTOR = new CanPort(9, "Canivore");
+    public static final CanPort BACK_LEFT_MODULE_STEER_ENCODER = new CanPort(15, "Canivore");
     public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(149.0625);// -Math.toRadians(30.673828125d);
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 13;
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 14;
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 18;
+    public static final CanPort BACK_RIGHT_MODULE_DRIVE_MOTOR = new CanPort(13, "Canivore");
+    public static final CanPort BACK_RIGHT_MODULE_STEER_MOTOR = new CanPort(14, "Canivore");
+    public static final CanPort BACK_RIGHT_MODULE_STEER_ENCODER = new CanPort(18, "Canivore");
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(59.765);// -Math.toRadians(119.00390625d);
 
 }
