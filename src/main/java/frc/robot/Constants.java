33fc99b8ab8b4fb07a7573a73abe76ab26aba11a
im-a-robo <nmdas05x@gmail.com>
2022-11-07 11:01:55 -0500

    changes to the zero pose of the swerve modules

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,7 +15,7 @@ public final class Constants {
     public static final double DRIVETRAIN_TRACKWIDTH_METERS = Units.inchesToMeters(22.375);
 
     public static final double DRIVETRAIN_WHEELBASE_METERS = Units.inchesToMeters(22.375);
-
+    
     public static final CanPort FRONT_LEFT_MODULE_DRIVE_MOTOR = new CanPort(8, "Canivore");
     public static final CanPort FRONT_LEFT_MODULE_STEER_MOTOR = new CanPort(7, "Canivore");
     public static final CanPort FRONT_LEFT_MODULE_STEER_ENCODER = new CanPort(16, "Canivore");
@@ -24,17 +24,17 @@ public final class Constants {
     public static final CanPort FRONT_RIGHT_MODULE_DRIVE_MOTOR = new CanPort(11, "Canivore");
     public static final CanPort FRONT_RIGHT_MODULE_STEER_MOTOR = new CanPort(12, "Canivore");
     public static final CanPort FRONT_RIGHT_MODULE_STEER_ENCODER = new CanPort(17, "Canivore");
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(195.447);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(193.447);// -Math.toRadians(-12.744140625d);
 
     public static final CanPort BACK_LEFT_MODULE_DRIVE_MOTOR = new CanPort(10, "Canivore");
     public static final CanPort BACK_LEFT_MODULE_STEER_MOTOR = new CanPort(9, "Canivore");
     public static final CanPort BACK_LEFT_MODULE_STEER_ENCODER = new CanPort(15, "Canivore");
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(246.961);// 68.178); //
-                                                                                        // -Math.toRadians(30.673828125d);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(68.178);
 
+    // Back right module
     public static final CanPort BACK_RIGHT_MODULE_DRIVE_MOTOR = new CanPort(13, "Canivore");
     public static final CanPort BACK_RIGHT_MODULE_STEER_MOTOR = new CanPort(14, "Canivore");
     public static final CanPort BACK_RIGHT_MODULE_STEER_ENCODER = new CanPort(18, "Canivore");
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(59.765);// 59.765);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(59.765);
 
 }
